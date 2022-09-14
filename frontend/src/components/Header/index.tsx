a684fb6fde35433a399cc8d1a5cea54f76c33258
Mbpicazzio <mbpicazzio@gmail.com>
2022-09-14 10:25:29 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo DSMeta" />
+                    <h1>DsMeta</h1>
+                    <p>
+                        Desenvolvido por:
+                        <a href="https://www.linkedin.com/in/mbpicazzio/">@mbpicazzio</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
